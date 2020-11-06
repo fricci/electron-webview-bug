d4212d24c99deeaac52664680b911f0f7f4ce1ed
Ferenc Czina <ferenc@czina.me>
2020-11-06 16:46:54 +0100

    bug reproduction

--- client-test.js
+++ client-test.js
@@ -1,12 +1,21 @@
 const webContent =
     `<html>
 
-<body>
-    <div id="result"></div>
-    <script src="file://C:/Users/feren/Projects/webview-sandbox/client-test-in-webview.js"></script>
-</body>
-
-</html>`;
+    <head>
+        <script>
+            console.log('loading script');
+            var script = document.createElement('script');
+            script.src = 'file://C:/Users/feren/Projects/webview-sandbox/client-test-in-webview.js';
+    
+            document.head.appendChild(script);
+        </script>
+    </head>
+    
+    <body>
+        <div id="result"></div>
+    </body>
+    
+    </html>`;
 
 const webview = document.createElement('webview');
 webview.setAttribute('nodeintegration', 'true');
@@ -14,6 +23,8 @@ webview.setAttribute('nodeintegrationinsubframes', 'true');
 webview.setAttribute('enableremotemodule', 'true');
 webview.setAttribute('disablewebsecurity', 'true');
 webview.setAttribute('webpreferences', 'allowRunningInsecureContent');
-webview.setAttribute('src', 'data:text/html;base64,' + window.btoa(webContent));
+//webview.setAttribute('src', 'file://C:/Users/feren/Projects/webview-sandbox/webview.html');
 
-document.getElementById('webViewPlace').appendChild(webview)
+webview.src = 'data:text/html;base64,' + window.btoa(webContent);
+
+document.getElementById('webViewPlace').appendChild(webview);
